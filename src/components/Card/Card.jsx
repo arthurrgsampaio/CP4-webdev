cfe1bed37904c5ae188dd1f45380a84994c405c9
Arthur Rodrigues <arthur.rgs03@gmail.com>
2024-08-29 20:13:38 -0300

    feat: Home + Header

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,14 +2,38 @@
 import "./card.css"
 
 const Card = ({backColor, title, icon, number, children}) => {
+    let color;
+    switch(backColor) {
+        case "red":
+            color = "#F1C2B0";
+            break;
+        case "yellow":
+            color = "#F9EB98";
+            break;
+        case "blue":
+            color = "#6975E8";
+            break;
+        case "green":
+            color = "#C0D9DD";
+            break;
+        default:
+            color = "#808080";
+            break;
+    }
+
+    const styles = {
+        backgroundColor: color
+    }
+    
+
     return (
-        <div style={{backgroundColor: {backColor}}}>
-            <div>
+        <div style={styles} className="card">
+            <div className="numberIcon"> 
                 <h2>{number}</h2>
                 <img src={icon} />
             </div>
             <h3>{title}</h3>
-            {children}
+            <p>{children}</p>
         </div>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,13 +5,16 @@ import Button from "../../components/Button/Button";
 import Hero from "../../assets/Hero image.png"
 import Card from "../../components/Card/Card";
 import Youtube from "../../assets/Youtube.jfif"
+import Tiktok from "../../assets/Tiktok.jfif"
+import Facebook from "../../assets/Facebook.jfif" 
+import Instagram from "../../assets/Instagram.jfif"
 
 const Home = () => {
     return(
         <div id="containerHome">
             <Header selected={"home"}/>
             
-            <div id="firstHomeContainer" style={{margin: "2rem 0"}}>
+            <div id="firstHomeContainer" style={{margin: "3rem 0"}}>
                 <div id="textContainer" style={{flex: "1"}}>
                     <Title>
                         Crie seus vídeos online
@@ -27,8 +30,17 @@ const Home = () => {
             </div>
 
             <div id="cardsContainer">
-                <Card backColor={"#F1C2B0"} icon={Youtube} number={"01"} title={"Youtube"}>
+                <Card backColor={"red"} icon={Youtube} number={"01"} title={"Youtube"}>
                     Produza conteúdo cativante e conquiste seu público com vídeos de alta qualidade no maior palco digital do mundo.
+                </Card>
+                <Card backColor={"yellow"} icon={Tiktok} number={"02"} title={"Tiktok"}>
+                    Faça sua criatividade brilhar em vídeos curtos e envolventes que vão cativar a comunidade global do TikTok.
+                </Card>
+                <Card backColor={"green"} icon={Facebook} number={"03"} title={"Facebook"}>
+                    Conecte-se com sua audiência de forma autêntica e impactante através de vídeos que se destacam no feed do Facebook. 
+                </Card>
+                <Card backColor={"blue"} icon={Instagram} number={"04"} title={"Instagram"}>
+                Compartilhe suas histórias de maneira única e conquiste milhões de likes no Instagram utilizando Stories e Reels.
                 </Card>
             </div>
         </div>
